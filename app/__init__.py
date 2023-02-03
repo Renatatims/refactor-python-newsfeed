028ec1740a76b48f4cddfd7d4675b796f6194fd3
ReTims <renatatims@gmail.com>
2023-02-03 14:24:49 -0600

    Added filters in Template Files - post-info, comments and edit-post

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from app.routes import home, dashboard
 from flask import Flask
 from app.db import init_db
+from app.utils import filters
 
 def create_app(test_config=None):
   # set up app config
@@ -12,9 +13,14 @@ def create_app(test_config=None):
   @app.route('/hello')
   def hello():
     return 'hello world'
-    # register routes
+  # register routes
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
+  
+  # register filter functions with the Jinja template environment
+  app.jinja_env.filters['format_url'] = filters.format_url
+  app.jinja_env.filters['format_date'] = filters.format_date
+  app.jinja_env.filters['format_plural'] = filters.format_plural
   
   init_db(app)
  
